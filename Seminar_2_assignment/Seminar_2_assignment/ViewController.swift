50c92b9170e71121ae2a41ab27e1601202170191
hyj422 <128811092+hyj422@users.noreply.github.com>
2023-04-15 17:39:22 +0900

    과제파일 생성

---
+++ Seminar_2_assignment/Seminar_2_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar_2_assignment
+//
+//  Created by 홍유정 on 2023/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
